aea8b412ec4b6f0005285e5bbf30ebc3bbf6f24d
Max Z <max.zlotskiy@gmail.com>
2017-10-17 19:12:53 -0400

    wrote getGrades

--- stu_mean.py
+++ stu_mean.py
@@ -1,20 +1,27 @@
+import db_builder
 import sqlite3
 import csv
 
 f="discobandit.db"
-db=sqlite3.connect(f)
-c=db.cursor()
+db = sqlite3.connect(f)
+c = db.cursor()
 
+# Creates database and tables from CSV
+def createGradebook():
+   db_builder.insertCSV(c)
+   c.execute("CREATE TABLE peeps_avg (id INTEGER, average REAL);")
+   db.commit()
+
+# Returns a list of grades for a student
 def getGrades(id):
-   temp=[]
-   c.execute("SELECT * FROM classes WHERE id = "+str(id)+";")
-   for row in c:
-       temp.append(row[1])
-   return temp
-    
-##command=""
-##c.execute(command)
-print(getGrades(1))
+   c.execute("SELECT mark FROM courses WHERE id = "+str(id)+";")
+   grades = []
+   for grade in c:
+      grades.append(grade[0])
+   return grades
+
+createGradebook()
+print getGrades(1)
 
 db.commit()
 db.close()
